869ad6949ad2d0164dda5ebd9e8a28862d531ebc
Owl <practice.tech2022@gmail.com>
2025-02-22 18:57:27 +0800

    Addressing deployment issues

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -5,13 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model
-{
+class Customer extends Model {
     use HasFactory;
 
     protected $table = 'customers';
 
     protected $fillable = ['first_name', 'last_name'];
 
-    public $timestamps = false; // Disable timestamps
 }
