93f478f3f16060840933c83cc4b7706c9fea8c70
Owl <practice.tech2022@gmail.com>
2025-02-23 01:52:35 +0800

    Fix 6

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomerController;
+use app\Http\Controllers\CustomerController;
 
 Route::get('/customers', [CustomerController::class, 'index']);
 Route::post('/customers', [CustomerController::class, 'store']);
