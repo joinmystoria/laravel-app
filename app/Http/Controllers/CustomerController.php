bb173fefd71e3c3e2ba47d2122a00ec565f2ef87
Owl <practice.tech2022@gmail.com>
2025-02-23 14:48:31 +0800

    Fix capital A prod issues

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Services\CustomerService;
+use App\Services\CustomerService;
 
 class CustomerController extends Controller {
     
